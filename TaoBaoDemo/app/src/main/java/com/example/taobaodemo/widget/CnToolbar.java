686f2bce97468693c9620fe0b63d0fb52bb1d3e5
刘六乾 <liuliuqian@yanxiu.com>
2019-10-30 16:03:29 +0800

    toolbar定义

---
+++ TaoBaoDemo/app/src/main/java/com/example/taobaodemo/widget/CnToolbar.java
@@ -0,0 +1,131 @@
+package com.example.taobaodemo.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.TintTypedArray;
+
+import com.example.taobaodemo.R;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class CnToolbar extends Toolbar {
+
+    private LayoutInflater mInflater;
+    private View mView;
+    private TextView mTextView;
+    private EditText mSearchView;
+    private ImageButton mRightImageButton;
+
+    public CnToolbar(Context context) {
+        this(context, null);
+    }
+
+    public CnToolbar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CnToolbar(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public CnToolbar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        initView();
+        setContentInsetsRelative(30,30);
+
+        if (attrs != null) {
+
+            final TintTypedArray a = TintTypedArray.obtainStyledAttributes(getContext(), attrs, R.styleable.CnToolbar, defStyleAttr, defStyleRes);
+            final Drawable rightIcon = a.getDrawable(R.styleable.CnToolbar_rightButtonIcon);
+            if (rightIcon != null) {
+                setRightButtonIcon(rightIcon);
+            }
+
+            boolean isShowSearchView = a.getBoolean(R.styleable.CnToolbar_isShowSearchView, false);
+            if (isShowSearchView) {
+                showSearchView();
+                hideTitleView();
+            }
+            a.recycle();
+        }
+
+    }
+
+    private void initView() {
+
+        if (mView == null) {
+            mInflater = LayoutInflater.from(getContext());
+            mView = mInflater.inflate(R.layout.toolbar, null);
+            mTextView = mView.findViewById(R.id.toolbar_title);
+            mSearchView = mView.findViewById(R.id.toolbar_searchview);
+            mRightImageButton = mView.findViewById(R.id.toolbar_rightButton);
+
+            LayoutParams lp = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER_HORIZONTAL);
+            addView(mView, lp);
+        }
+
+    }
+
+    public void setRightButtonIcon(Drawable icon) {
+        if (mRightImageButton != null) {
+            mRightImageButton.setImageDrawable(icon);
+            mRightImageButton.setVisibility(VISIBLE);
+        }
+    }
+
+    public void setRightButtonOnClickListener(OnClickListener li) {
+        mRightImageButton.setOnClickListener(li);
+    }
+
+    @Override
+    public void setTitle(int resId) {
+        setTitle(getContext().getText(resId));
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        initView();
+        if (mTextView != null) {
+            mTextView.setText(title);
+            showTitleView();
+        }
+    }
+
+
+    public void showSearchView() {
+        if (mSearchView != null) {
+            mSearchView.setVisibility(VISIBLE);
+        }
+    }
+
+    public void hideSearchView() {
+        if (mSearchView != null) {
+            mSearchView.setVisibility(GONE);
+        }
+    }
+
+    public void showTitleView() {
+        if (mTextView != null) {
+            mTextView.setVisibility(VISIBLE);
+        }
+    }
+
+    public void hideTitleView() {
+        if (mTextView != null) {
+            mTextView.setVisibility(GONE);
+        }
+    }
+}
