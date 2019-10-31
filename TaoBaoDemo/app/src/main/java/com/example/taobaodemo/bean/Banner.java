e63eeca4e3f4f90f5570c4719bf1e78292662e96
刘六乾 <liuliuqian@yanxiu.com>
2019-10-31 11:10:01 +0800

    首页接口数控请求

---
+++ TaoBaoDemo/app/src/main/java/com/example/taobaodemo/bean/Banner.java
@@ -0,0 +1,32 @@
+package com.example.taobaodemo.bean;
+
+public class Banner extends BaseBean {
+
+    private String name;
+    private String imgUrl;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- TaoBaoDemo/app/src/main/java/com/example/taobaodemo/fragment/HomeFragment.java
+++ TaoBaoDemo/app/src/main/java/com/example/taobaodemo/fragment/HomeFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +17,24 @@ import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.example.taobaodemo.R;
 import com.example.taobaodemo.adapter.HomeCatgoryAdapter;
+import com.example.taobaodemo.bean.Banner;
 import com.example.taobaodemo.bean.HomeCategory;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,20 +47,58 @@ public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
     private HomeCatgoryAdapter adapter;
 
+    Gson gson = new Gson();
+    List<Banner> mBanner;
+    Handler mHandler = new Handler(Looper.getMainLooper());
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         rootView = inflater.inflate(R.layout.fragment_home, container, false);
         initView();
+        getBannerData();
         return rootView;
     }
 
     private void initView() {
         sliderShow = rootView.findViewById(R.id.slider);
         recyclerView = rootView.findViewById(R.id.recyclerview);
-        initSlider();
+
         initRecyclerView();
+
+    }
+
+    private void getBannerData() {
+
+        String url = "http://112.124.22.238:8081/course_api/banner/query?type=1";
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                String json = response.body().string();
+                Type type = new TypeToken<List<Banner>>() {
+                }.getType();
+                mBanner = gson.fromJson(json, type);
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        initSlider();
+                    }
+                });
+            }
+        });
 
     }
 
@@ -52,46 +106,36 @@ public class HomeFragment extends Fragment {
 
         List<HomeCategory> datas = new ArrayList<>(15);
 
-        HomeCategory category = new HomeCategory("热门活动",R.drawable.img_big_1,R.drawable.img_1_small1,R.drawable.img_1_small2);
+        HomeCategory category = new HomeCategory("热门活动", R.drawable.img_big_1, R.drawable.img_1_small1, R.drawable.img_1_small2);
         datas.add(category);
 
-        category = new HomeCategory("有利可图",R.drawable.img_big_4,R.drawable.img_4_small1,R.drawable.img_4_small2);
+        category = new HomeCategory("有利可图", R.drawable.img_big_4, R.drawable.img_4_small1, R.drawable.img_4_small2);
         datas.add(category);
-        category = new HomeCategory("品牌街",R.drawable.img_big_2,R.drawable.img_2_small1,R.drawable.img_2_small2);
-        datas.add(category);
-
-        category = new HomeCategory("金融街 包赚翻",R.drawable.img_big_1,R.drawable.img_3_small1,R.drawable.imag_3_small2);
+        category = new HomeCategory("品牌街", R.drawable.img_big_2, R.drawable.img_2_small1, R.drawable.img_2_small2);
         datas.add(category);
 
-        category = new HomeCategory("超值购",R.drawable.img_big_0,R.drawable.img_0_small1,R.drawable.img_0_small2);
+        category = new HomeCategory("金融街 包赚翻", R.drawable.img_big_1, R.drawable.img_3_small1, R.drawable.imag_3_small2);
         datas.add(category);
 
-        adapter = new HomeCatgoryAdapter(getContext(),datas);
+        category = new HomeCategory("超值购", R.drawable.img_big_0, R.drawable.img_0_small1, R.drawable.img_0_small2);
+        datas.add(category);
+
+        adapter = new HomeCatgoryAdapter(getContext(), datas);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     private void initSlider() {
-        TextSliderView textSliderView = new TextSliderView(getContext());
-        textSliderView
-                .description("Game of Thrones")
-                .image("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1572424824&di=3c561ec4443aabc223049822c71a7de4&src=http://5b0988e595225.cdn.sohucs.com/images/20180425/eba53f4eb48e4c81a9de070577dc287c.jpeg");
 
-        sliderShow.addSlider(textSliderView);
+        if (mBanner == null) {
+            return;
+        }
+        for (Banner banner : mBanner) {
+            TextSliderView textSliderView = new TextSliderView(getContext());
+            textSliderView.description(banner.getName()).image(banner.getImgUrl());
+            sliderShow.addSlider(textSliderView);
+        }
 
-        TextSliderView textSliderView2 = new TextSliderView(getContext());
-        textSliderView2
-                .description("Game of Thrones")
-                .image("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572434917577&di=559ea449550666ec9d420def2eadae55&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6b0018f329217b50b48d9478776a24c9825cbef7cd86-IrED2R_fw658");
-
-        sliderShow.addSlider(textSliderView2);
-
-        TextSliderView textSliderView3 = new TextSliderView(getContext());
-        textSliderView3
-                .description("Game of Thrones")
-                .image("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572434917576&di=e41b42efc6c2a9e78bd9626b0ef38db0&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fb4364e79ca2797d74b8134b4bb48fea40af428ef48d40-imG7pw_fw658");
-
-        sliderShow.addSlider(textSliderView3);
     }
 
 }
